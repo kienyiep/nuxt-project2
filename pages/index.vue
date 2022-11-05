2b586f48020338c06c91ac726461a9881427d322
Pong Kien Yiep <kienyiep@gmail.com>
2022-11-05 21:51:15 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview
+        id="1"
+        thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
+        title="Hello there!"
+        previewText="This is my first post!"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
+        title="Hello there!"
+        previewText="This is my second post!"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
+        title="Hello there!"
+        previewText="This is my third post!"
+      />
+
+      <!-- <nuxt-link :to="'/posts/' + 1" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="
+              background-image: url(https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg);
+            "
+          ></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+      <nuxt-link :to="'/posts/' + 2" class="post-preview">
+        <article>
+          <div
+            class="post-thumbnail"
+            style="
+              background-image: url(https://1.bp.blogspot.com/-KjIf2wAQiWo/YQAygaAixVI/AAAAAAAAJnA/wPP2USKPJGU4CMrXXjw7Fp9vkRzvknqPgCPcBGAsYHg/w919-h516-p-k-no-nu/genshin-impact-raiden-shogun-art-4k-uhdpaper.com-943.1_a-thumbnail.jpg);
+            "
+          ></div>
+          <div class="post-content">
+            <h1>Post Title 2</h1>
+            <p>Preview Text 2</p>
+          </div>
+        </article>
+      </nuxt-link> -->
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreviews from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview: PostPreviews,
+  },
+};
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-image: url("~assets/images/genshin-impact.jpg");
+  ''background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+/* .post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+} */
+</style>
