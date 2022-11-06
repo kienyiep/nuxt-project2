390f09e23934127dc51e1fbc88247799af044f89
Pong Kien Yiep <kienyiep@gmail.com>
2022-11-06 13:11:37 +0800

    adminPostForm

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
       title="Hello there!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
       title="Hello there!"
       previewText="This is my second post!"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://www.pcgamesn.com/wp-content/sites/pcgamesn/2022/10/genshin-impact-best-nahida-build.jpg"
       title="Hello there!"
       previewText="This is my third post!"
@@ -27,12 +30,12 @@ export default {
   components: {
     PostPreview: PostPreviews,
   },
-  // props: {
-  //   isAdmin: {
-  //     type: Boolean,
-  //     default: false,
-  //   },
-  // },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 <style scoped>
